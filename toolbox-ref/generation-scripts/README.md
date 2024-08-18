c4b7d27a4b80d22b72be087a753ea112dff3be7f
Dagen Brock <dagenbrock@gmail.com>
2024-08-17 20:14:33 -0500

    adding website version

---
+++ toolbox-ref/generation-scripts/README.md
@@ -0,0 +1,5 @@
+# Scripts to generate docs
+
+1. (SKIP!) run `cleantxt.sh > ../toolcalls.json` to get started if in ye olden times and make a base json
+2. run `python gentxt.py > ../toolcalls_by_tool.txt` to build txt file
+3. run `python gentxt.py > ../toolcalls.yml` to build yaml file
--- toolbox-ref/generation-scripts/cleantxt.sh
+++ toolbox-ref/generation-scripts/cleantxt.sh
@@ -1,8 +1,8 @@
 # this cleans up the cdrom text file from antoine, but it depends on consistent spacing
 # could replace with something that has a regex for all the tool names
 grep  '^\$' textfile.txt | tr "\"" "'" | awk '{
-  toolcall = substr($0, 1, 3);
-  toolnum = substr($0, 5, 3);
+  toolcall = substr($0, 2, 2);
+  toolnum = substr($0, 6, 2);
   toolname = substr($0, 10, 15);
   callname = substr($0, 27, 15);
   description = substr($0, 49);
